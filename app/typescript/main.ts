ef6bad7acf665206c68bf27b6210500130b4f7ea
ohle <joclaussen@gmail.com>
2016-01-15 16:52:16 +0100

    Layout iteration

--- app/typescript/main.ts
+++ app/typescript/main.ts
@@ -1,17 +1,3 @@
 /// <reference path="../typings/jquery/jquery.d.ts"/>
 declare var $: JQueryStatic;
 
-class Greeter {
-   constructor(public greeting : string) { }
-   greet() {
-       window.alert(this.greeting);
-   }
-};
-
-var greeter = new Greeter("Boo!");
-greeter.greet();
-
-// testing typescriptized jquery
-var foo : JQuery = $('body');
-console.log(foo.html());
-alert(foo.html());
