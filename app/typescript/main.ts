bdc7dbe138d5652482be9fb1e4688d7d52771b87
ohle <joclaussen@gmail.com>
2016-01-18 13:26:41 +0100

    Some buttons for adding/removing test notes for layouting purposes

--- app/typescript/main.ts
+++ app/typescript/main.ts
@@ -1,3 +1,13 @@
 /// <reference path="../typings/jquery/jquery.d.ts"/>
 declare var $: JQueryStatic;
-
+$(() => {
+    var plusButton: JQuery = $("<button>+</button>");
+    var minusButton: JQuery = $("<button>-</button>");
+    $("header").append(plusButton).append(minusButton);
+    plusButton.on("click", () => {
+        $("ul.notes").append("<li> autonote </li>");
+    });
+    minusButton.on("click", () => {
+        $("ul.notes").children().last().remove();
+    });
+});
