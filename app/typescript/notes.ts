035631f6124553ba825ebaeef0ea5075b8cf6b33
ohle <joclaussen@gmail.com>
2016-01-28 12:01:51 +0100

    Implementation stub for FilteredNotes

--- app/typescript/notes.ts
+++ app/typescript/notes.ts
@@ -1,4 +1,5 @@
 /// <reference path="../typings/backbone/backbone.d.ts"/>
+/// <reference path="queries.ts"/>
 
 module Notes {
     import bb = Backbone;
@@ -27,6 +28,22 @@ module Notes {
 
         setBody(b : string) {
             super.set("body", b);
+        }
+    }
+
+    /**
+     * A Collection that acts as a proxy for a Collection<NoteModel>
+     * and takes a Collection<QueryModel> to be used for filtering the notes
+     */
+    export class FilteredNotes extends bb.Collection<NoteModel> {
+        queries : bb.Collection<Queries.QueryModel>;
+        notes :  bb.Collection<NoteModel>;
+
+        constructor(queries : bb.Collection<Queries.QueryModel>,
+                    notes : bb.Collection<NoteModel>) {
+            super();
+            this.queries = queries;
+            this.notes = notes;
         }
     }
 }
