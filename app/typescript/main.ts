8e9e545a8dba2740a0d98a5b749405b6b4b45f05
ohle <joclaussen@gmail.com>
2016-02-14 21:16:15 +0100

    Use dualStorage

--- app/typescript/main.ts
+++ app/typescript/main.ts
@@ -1,27 +1,30 @@
 /// <reference path="../typings/jquery/jquery.d.ts"/>
 /// <reference path="notes.ts"/>
 /// <reference path="queries.ts"/>
+///<reference path="backbone.dualStorage.d.ts"/>
 
 import q = Queries;
 import n = Notes;
 
-declare var $: JQueryStatic;
+// Temporarily add 404 as offline marker so we can set bogus URLs and have
+// localStorage syncing work. Might still use this technique later when no
+// backend is selected, e.g.
+Backbone.DualStorage.offlineStatusCodes.push(404);
 
-let note1 = new n.NoteModel("title", "body");
-let note2 = new n.NoteModel("foo", "bar");
+declare var $: JQueryStatic;
 
 let qs = new Backbone.Collection<q.QueryModel>();
 
 let aq = new q.QueriesView({collection : qs, el: $('header')});
 
 let notes = new Backbone.Collection<n.NoteModel>();
-notes.add(note1);
-notes.add(note2);
-note2.setEditing(true);
+notes.url = "foo"; // bogus for now
+notes.fetch();
+
 let filtered = new n.FilteredNotes(qs, notes);
 let nv = new n.NotesView({ collection : filtered, el: $('ul.notes') });
 
 $('#addNote').on("click", () => { 
     let newNote = new n.NoteModel("title", "body");
-    notes.add(newNote, { at: 0 });
+    notes.create(newNote);
 });
