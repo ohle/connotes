bc37289a26f29d111feef7704a88bef5554376f9
ohle <joclaussen@gmail.com>
2016-02-22 21:44:08 +0100

    Use dedicated view for filtered counting

--- app/typescript/main.ts
+++ app/typescript/main.ts
@@ -29,7 +29,9 @@ class NoteCollection extends Backbone.Collection<n.NoteModel> {
 let notes = new NoteCollection("foo"); // bogus URL for now
 notes.fetch();
 
-let filtered = new n.FilteredNotes(qs, notes);
+let cm = new n.FilteredCount();
+let filtered = new n.FilteredNotes(qs, notes, cm);
+let cv = new n.FilteredCountView({ model: cm, el: $('#filteredCount') });
 let nv = new n.NotesView({ collection : filtered, el: $('ul.notes') });
 
 $('#addNote').on("click", () => { 
